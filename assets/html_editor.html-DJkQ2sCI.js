31ce7d2cb3b6e86595ceef36ad67be512712d8bc
jet-w <unisa.dady@gmail.com>
2025-10-07 23:43:43 +0000

    Update from https://github.com/jet-w/jet-w.blog/commit/03cab24148ec1206f6ee8098edfa3a9605f3c533

---
+++ assets/html_editor.html-DJkQ2sCI.js
@@ -0,0 +1,20 @@
+import{_ as t}from"./plugin-vue_export-helper-DlAUqK2U.js";import{c as e,f as o,o as r}from"./app-DxHhAbrN.js";const n={};function i(a,c){return r(),e("div",null,[o(` 
+<script setup lang="js">
+import HtmlEditor from "@HtmlEditor";
+import { ref } from 'vue';
+
+const html = ref(\`
+<html>
+  <head></head>
+  <body>
+    <div>Hello World !!</div>
+  </body>
+</html>\`);
+
+const css = ref(\`div { 
+  color: blue; 
+  background: red
+}
+\`);
+<\/script>
+<HtmlEditor :html_code="html" :css_code="css"></HtmlEditor> `)])}const d=t(n,[["render",i]]),s=JSON.parse('{"path":"/techniques/archive/coding/front-end/css/html_editor.html","title":"HTML Editor","lang":"en-US","frontmatter":{"title":"HTML Editor","date":"2023-11-05T00:00:00.000Z","icon":"circle-dot","author":"Haiyue","category":["make"],"tag":["skills"],"star":false,"sticky":false,"head":[["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"HTML Editor\\",\\"image\\":[\\"\\"],\\"datePublished\\":\\"2023-11-05T00:00:00.000Z\\",\\"dateModified\\":\\"2025-09-20T05:10:40.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"Haiyue\\"}]}"],["meta",{"property":"og:url","content":"https://jet-w.github.io/techniques/archive/coding/front-end/css/html_editor.html"}],["meta",{"property":"og:site_name","content":"Mr. Dream\'s Blog"}],["meta",{"property":"og:title","content":"HTML Editor"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"en-US"}],["meta",{"property":"og:updated_time","content":"2025-09-20T05:10:40.000Z"}],["meta",{"property":"article:author","content":"Haiyue"}],["meta",{"property":"article:tag","content":"skills"}],["meta",{"property":"article:published_time","content":"2023-11-05T00:00:00.000Z"}],["meta",{"property":"article:modified_time","content":"2025-09-20T05:10:40.000Z"}]]},"git":{"createdTime":1727616737000,"updatedTime":1758345040000,"contributors":[{"name":"Haiyue","username":"Haiyue","email":"nutterair1989@gmail.com","commits":7,"url":"https://github.com/Haiyue"},{"name":"jet","username":"jet","email":"unisa.dady@gmail.com","commits":1,"url":"https://github.com/jet"}]},"readingTime":{"minutes":0.2,"words":60},"filePathRelative":"techniques/archive/coding/front-end/css/html_editor.md","excerpt":"<!-- \\n<script setup lang=\\"js\\">\\nimport HtmlEditor from \\"@HtmlEditor\\";\\nimport { ref } from \'vue\';\\n\\nconst html = ref(`\\n<html>\\n  <head></head>\\n  <body>\\n    <div>Hello World !!</div>\\n  </body>\\n</html>`);\\n\\nconst css = ref(`div { \\n  color: blue; \\n  background: red\\n}\\n`);\\n<\/script>\\n<HtmlEditor :html_code=\\"html\\" :css_code=\\"css\\"></HtmlEditor> -->"}');export{d as comp,s as data};
