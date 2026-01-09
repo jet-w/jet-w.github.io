9659fa2ce943c7dc2e752d2e745f923726b886ad
jet-w <unisa.dady@gmail.com>
2026-01-09 10:43:20 +0000

    Update from https://github.com/jet-w/jet-w.blog/commit/1df76177d330ec9b99e7838412377552f0c2614c

--- assets/about.QOvPnAXp.js
+++
@@ -1,8 +0,0 @@
-async function getMod() {
-						return import('./about.C69pX1o_.js');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts: [] };
-
-export { defaultMod as default };
