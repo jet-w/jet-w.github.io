7d7b032f23cbd37eae1c6fa81471a7c16517877e
jet-w <unisa.dady@gmail.com>
2026-01-11 07:57:55 +0000

    Update from https://github.com/jet-w/jet-w.blog/commit/eea608a11c239c20c87f09558aff0a916ef95aae

--- assets/content-modules.SM37-9ma.js
+++
@@ -1,7 +0,0 @@
-const contentModules = new Map([
-["content/pages/about.mdx", () => import('./about.B7ZDbUWM.js')],
-["content/pages/index.mdx", () => import('./index.PGUwUoYC.js')],
-["content/posts/blog_docs/12-slides.mdx", () => import('./12-slides.BHXjTBN5.js')],
-["content/posts/test/test-echarts.mdx", () => import('./test-echarts.DN53OQFh.js')]]);
-
-export { contentModules as default };
